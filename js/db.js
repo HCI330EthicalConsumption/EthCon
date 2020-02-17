fc13f6cb6d8613650bfa3a3dc9311f4dfba42533
jtschuster <jacksontschuster@gmail.com>
2020-02-17 16:59:53 -0600

    more searching code

--- js/db.js
+++ js/db.js
@@ -1,9 +1,10 @@
-// var fetch = require('node-fetch');
+// let fetch = require('node-fetch');
 const get = async (url) => {
     console.log(url);
     const proxyurl = "https://cors-anywhere.herokuapp.com/";
+    // const proxyurl = "https://localhost:8080/";
+    console.log(proxyurl + url);
     let data = await fetch(proxyurl + url);
-    console.log(data);
     return data;
 }
 // takes in keywords to search for and returns a list of product json objects. Returns up to 21 products.
@@ -24,17 +25,34 @@ const search_gg = async (keywords, sortby, pagenum) => {
         pagenum = "1";
     }
     const url = "https://www.goodguide.com/catalog/search.json?filter=" + keywords + "&sort=" + sort_term + "&page=" + pagenum;
+
     const resp = await get(url);
     console.log(resp);
     let data = await resp.json();
     console.log(data);
     return data.products;
 }
+//Not useable yet
 const search_wal = async (keywords) => {
     const url = "https://www.walmart.com/search/?query=" + keywords;
     const resp = await get(url);
     console.log(resp);
-    let data = await resp.json();
+    let data2 = await resp.text();
+    console.log(data2);
+    var htmlObject = document.createElement('div');
+    htmlObject.innerHTML = data2;
+    let interesting_stuff = htmlObject.getElementsByClassName("search-result-gridview-items");
+    htmlObject.innerHTML = data2;
+    let imgs = htmlObject.getElementsByTagName("img");
+    console.log(imgs);
+    for (img of imgs) {
+        console.log(img.src);
+    }
+    console.log(interesting_stuff[0].getElementsByClassName(""));
+    let parser = new DOMParser();
+    let xmlDoc = parser.parseFromString(interesting_stuff, "application/xml");
+    console.log(xmlDoc);
+    let data = xmlToJson(xmlDoc);
     console.log(JSON.stringify(data));
     return data;
 }
@@ -45,6 +63,7 @@ document.querySelector('#go').onclick = async () => {
     console.log(sortby);
     search_terms = document.getElementById("search-terms").value
     data = await search_gg(search_terms, sortby, 1);
+    // data = await search_wal(search_terms);
     console.log(JSON.stringify(data));
     document.getElementById("place").innerHTML += JSON.stringify(data);
 };
