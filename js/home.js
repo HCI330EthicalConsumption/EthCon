b6286f7d81e90b977589790e913f559dfa2a95c3
jtschuster <jacksontschuster@gmail.com>
2020-03-04 23:51:53 -0600

    sadfsafsdfdsflkjs idk

--- js/home.js
+++ js/home.js
@@ -10,8 +10,6 @@ document.querySelector('#login-username').onkeypress = async (event) => {
     enter_pressed = search_on_enter(event); //determine if enter was the key pressed
     if (enter_pressed) { // if so, get products and load them
         let username = document.querySelector("#login-username").value;
-        let resp = await get_user_info_from_username(username);
-        console.log(resp);
         if (await get_user_info_from_username(username)) {
             open_home_page();
         }
@@ -26,9 +24,13 @@ document.querySelector("#signup-button").onclick = async (event) => {
 document.querySelector('#signup-username').onkeypress = async (event) => {
     enter_pressed = search_on_enter(event); //determine if enter was the key pressed
     if (enter_pressed) { // if so, get products and load them
+        console.log("about to make user");
         let username = document.querySelector("#signup-username").value;
+        // console.log(await make_user(username));
         if (await make_user(username)) {
+            console.log("user is made");
             open_home_page();
         }
+        console.log("returned not true");
     };
 };
